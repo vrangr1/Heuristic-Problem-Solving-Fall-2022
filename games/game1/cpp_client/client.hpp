162b46e82d5eac0a049d13bd7cbca45fee558969
anavp <anavprasadanav@gmail.com>
2022-09-14 21:33:26 -0400

    removed extraneous files

--- games/game1/cpp_client/client.hpp
+++ games/game1/cpp_client/client.hpp
@@ -2,24 +2,12 @@
 #define CLIENT_HPP_
 
 #include <iostream>
-#include <fstream>
 #include <vector>
-#include <string.h>
-#include <ctype.h>
-#include <stdlib.h>
-#include <map>
-#include <iterator>
-#include <iomanip>
 #include <algorithm>
-#include <climits>
-#include <numeric>
 #include <cmath>
-#include <queue>
-#include <unordered_map>
 #include <bitset>
 #include <chrono>
 #include <assert.h>
-#include "loss_heuristics.hpp"
 using namespace std;
 
 #define endl "\n"
@@ -29,15 +17,7 @@ using namespace std;
 #define print_iter(x) cout << "*" << #x << ": " << *x << endl;
 #define space " "
 
-typedef long long int ll;
-typedef unsigned long long int ull;
 typedef short int si;
-template <typename type> void print(const vector<vector<type> > &arr);
-template <typename type> void print(const vector<type> &arr);
-template <typename type> void off_print(const vector<type> &arr);
-template <typename t1, typename t2> void print(const vector<pair<t1,t2> > &arr);
-template <typename t1, typename t2> void print(const vector<vector<pair<t1,t2> > > &arr);
-template <typename t1, typename t2> void print(const pair<t1, t2> &p);
 #define NOT_PROCESSED -1
 #define LOSS 0
 #define debug_mode false
@@ -145,7 +125,7 @@ class Player{
         const LOSS_STRATEGY strategy = BIGGEST;
     
     int random_loss_move(){
-        
+        return 0;
     }
 
     int biggest_loss_move(){
@@ -269,66 +249,4 @@ class Player{
             return move;
         }
 };
-
-template <typename type> void print(const vector<vector<type> > &arr){
-	cout << "\n[";
-	for(int i; i < arr.size(); ++i){
-		cout << "[";
-		for(int j; j < arr[i].size() - 1; ++j)
-			cout << arr[i][j] << ", ";
-		cout << arr[i][arr[i].size() - 1] << "]";
-		if (i != arr.size() - 1)
-			cout << "," << endl;
-	}
-	cout << "], 2D Vector\n";
-}
-
-
-template <typename type> void print(const vector<type> &arr){
-	cout << "\n[";
-	for(int i; i < arr.size(); ++i){
-		cout << arr[i];
-		if (i != arr.size() - 1)
-			cout << ", ";
-	}
-	cout << "], 1D Vector\n";
-}
-
-template <typename type> void off_print(const vector<type> &arr){
-	for(int i; i < arr.size(); ++i)
-		cout << arr[i] << " ";
-	cout << endl;
-}
-
-
-template <typename t1, typename t2> void print(const vector<pair<t1,t2> > &arr){
-	int n = arr.size();
-	cout << "\n[";
-	for(int i; i < n - 1; ++i)
-		cout << "{" << arr[i].first << "," << arr[i].second << "}, ";
-	cout << "{" << arr[n - 1].first << "," << arr[n - 1].second << "}], 1D Vector of Pairs\n";
-}
-
-
-template <typename t1, typename t2> void print(const vector<vector<pair<t1,t2> > > &arr){
-	cout << "\n[";
-	for(int i; i < arr.size(); ++i){
-		cout << "[";
-		for(int j; j < arr[i].size(); ++j){
-			cout << "{" << arr[i][j].first << "," << arr[i][j].second << "}";
-			if (j != arr[i].size() - 1)
-				cout << ", ";
-		}
-		cout << "]";
-		if (i != arr.size() - 1)
-			cout << "," << endl;
-	}
-	cout << "], 2D Vector of Pairs\n";
-}
-
-
-template <typename t1, typename t2> void print(const pair<t1, t2> &p){
-	cout << "\n{" << p.first << "," << p.second << "}, Pair\n";
-}
-
 #endif
