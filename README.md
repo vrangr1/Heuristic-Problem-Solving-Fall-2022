f92e95cc51dedafe19b91fb5ff9f49120bb0e233
anavp <anavprasadanav@gmail.com>
2022-10-07 14:34:51 -0400

    Let the Bandit Game commence!

--- README.md
+++ README.md
@@ -12,3 +12,28 @@ After cloning the repo, to properly update all the submodules cloned inside this
 * Game 1: Card Nim
     - Game Description: [link](https://cs.nyu.edu/courses/fall22/CSCI-GA.2965-001/cardnim.html)
     - Game Architecture: [HUNTINGHOUND/cardnimarchitect](https://github.com/HUNTINGHOUND/cardnimarchitect)
+* Game 2: Optimal Touring
+    - Game Description: [link](https://cs.nyu.edu/courses/fall22/CSCI-GA.2965-001/tour.html)
+    - Game Architecture: [Awesalot/optimalTourningArchitect](https://github.com/Awesalot/optimalTouringArchitect)
+* Game 3: No Tipping Game
+    - Game Description: [link](https://cs.nyu.edu/courses/fall22/CSCI-GA.2965-001/notipping.html)
+    - We were the architects for this game.
+    - Our architecture: [vrangr1/No-Tipping-Game-Architecture](https://github.com/vrangr1/No-Tipping-Game-Architecture)
+* Game 4: Ambulance Pickup
+    - Game Description: [link](https://cs.nyu.edu/courses/fall22/CSCI-GA.2965-001/ambulance.html)
+    - Game Architecture: [kanika127/HPS-ambulance-pickup-architecture](https://github.com/kanika127/HPS-ambulance-pickup-architecture)
+* Game 5: The Bandit Game
+    - Game Description: [link](https://cs.nyu.edu/courses/fall22/CSCI-GA.2965-001/bandit.html)
+    - Game Architecture: TBR
+* Game 6: Evasion
+    - Game Description: [link](https://cs.nyu.edu/courses/fall22/CSCI-GA.2965-001/evasion.html)
+    - Game Architecture: TBR
+* Game 7: Hidden Tunnel / Dig That
+    - Game Description: [link](https://cs.nyu.edu/courses/fall22/CSCI-GA.2965-001/digthatcomp.html)
+    - Game Architecture: TBR
+* Game 8: The Dating Game
+    - Game Description: [link](https://cs.nyu.edu/courses/fall22/CSCI-GA.2965-001/dating.html)
+    - Game Architecture: TBR
+* Game 9: Submarine Hunting
+    - Game Description: [link](https://cs.nyu.edu/courses/fall22/CSCI-GA.2965-001/subhunt.html)
+    - Game Architecture: TBR
---
+++ games/game5/setup.hpp
@@ -0,0 +1,196 @@
+#ifndef _SETUP_HPP_
+#define _SETUP_HPP_
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <map>
+#include <iterator>
+#include <iomanip>
+#include <algorithm>
+#include <climits>
+#include <numeric>
+#include <cmath>
+#include <queue>
+#include <unordered_map>
+#include <bit>
+#include <bitset>
+#include <assert.h>
+
+using namespace std;
+
+#define endl "\n"
+#define fastIO ios_base::sync_with_stdio(false),cin.tie(0)
+#define TEST int T;cin>>T;while(T--)solve();
+#define TEST1 solve();
+#define pass (void)0
+#define print_var(x) cout << #x << ": " << x << "\n";
+#define print_iter(x) cout << "*" << #x << ": " << *x << endl;
+#define space " "
+#define NOT_DEFINED -12923
+
+#define debug_mode false
+
+typedef long long int ll;
+typedef unsigned long long int ull;
+template <typename type> void print(const vector<vector<type> > &arr);
+template <typename type> void print(const vector<type> &arr);
+template <typename type> void off_print(const vector<type> &arr);
+template <typename t1, typename t2> void print(const vector<pair<t1,t2> > &arr);
+template <typename t1, typename t2> void print(const vector<vector<pair<t1,t2> > > &arr);
+template <typename t1, typename t2> void print(const pair<t1, t2> &p);
+
+#define NORMAL_SLOT 0.47
+#define WINNING_SLOT 0.6
+
+enum Player{
+    CASINO,
+    GAMBLER
+};
+
+
+class BanditGame{
+private:
+    int winning_slot, tokens, gambler_slot, pulls, previous_slot;
+public:
+    int s, k;
+    BanditGame(int s, int k){
+        this->s = s;
+        this->k = k;
+        assert(s <= 100);
+        assert(k <= s/7);
+        this->winning_slot = NOT_DEFINED;
+        this->gambler_slot = NOT_DEFINED;
+        this->previous_slot = NOT_DEFINED;
+        this->tokens = 500 * this->s;
+        this->pulls = 0;
+    }
+
+    inline int get_tokens(){
+        return this->tokens;
+    }
+
+    void casino_move(int choice, bool debug = false){
+        if (debug) print_var(choice);
+        if (this->winning_slot == NOT_DEFINED) this->winning_slot = choice; // game start
+        else if (this->winning_slot != choice && k-- > 0) this->winning_slot = choice;
+        else if (this->winning_slot != choice) assert(false); // Illegal move by casino
+        else pass;// this->winning_slot == choice
+    }
+
+    bool changed_gambling_slot(){
+        if (this->previous_slot != NOT_DEFINED)
+            return this->gambler_slot != this->previous_slot;
+        return false;
+    }
+
+    void gambler_move(int bet, int slot, bool debug = false){
+        if (bet == 0) game_over();
+        assert(bet > 0 && bet <= 3);
+        assert(slot >= 1 && slot <= this->s);
+        assert(this->winning_slot != NOT_DEFINED);
+        this->pulls++;
+        this->previous_slot = this->gambler_slot;
+        this->gambler_slot = slot;
+        if (debug) print_var(slot);
+        double predict = rand();
+        predict /= RAND_MAX;
+        // 0 <= predict <= 1
+        bet *= (predict <= (slot == this->winning_slot? WINNING_SLOT : NORMAL_SLOT)? 1 : -1);
+        if (debug){
+            print_var(predict);
+            print_var(bet);
+        }
+        this->tokens += bet;
+        cout << "After Gambler's move, the tokens remaining are: " << this->tokens << endl;
+        cout << endl;
+        if (this->pulls >= 100 * this->s) this->game_over();
+    }
+
+    void game_over(){
+        this->~BanditGame();
+    }
+
+    ~BanditGame(){
+        cout << "Game over!\n";
+        cout << "Gambler's tokens: " << this->tokens << endl;
+        cout << "Exiting now...\n";
+        exit(0);
+    }
+};
+
+
+
+
+
+
+
+
+
+
+template <typename type> void print(const vector<vector<type> > &arr){
+	cout << "\n[";
+	for (int i = 0; i < arr.size(); ++i){
+		cout << "[";
+		for (int j = 0; j < arr[i].size() - 1; ++j)
+			cout << arr[i][j] << ", ";
+		cout << arr[i][arr[i].size() - 1] << "]";
+		if (i != arr.size() - 1)
+			cout << "," << endl;
+	}
+	cout << "], 2D Vector\n";
+}
+
+
+template <typename type> void print(const vector<type> &arr){
+	cout << "\n[";
+	for (int i = 0; i < arr.size(); ++i){
+		cout << arr[i];
+		if (i != arr.size() - 1)
+			cout << ", ";
+	}
+	cout << "], 1D Vector\n";
+}
+
+template <typename type> void off_print(const vector<type> &arr){
+	for (int i = 0; i < arr.size(); ++i)
+		cout << arr[i] << " ";
+	cout << endl;
+}
+
+
+template <typename t1, typename t2> void print(const vector<pair<t1,t2> > &arr){
+	int n = arr.size();
+	cout << "\n[";
+	for (int i = 0; i < n - 1; ++i)
+		cout << "{" << arr[i].first << "," << arr[i].second << "}, ";
+	cout << "{" << arr[n - 1].first << "," << arr[n - 1].second << "}], 1D Vector of Pairs\n";
+}
+
+
+template <typename t1, typename t2> void print(const vector<vector<pair<t1,t2> > > &arr){
+	cout << "\n[";
+	for (int i = 0; i < arr.size(); ++i){
+		cout << "[";
+		for (int j = 0; j < arr[i].size(); ++j){
+			cout << "{" << arr[i][j].first << "," << arr[i][j].second << "}";
+			if (j != arr[i].size() - 1)
+				cout << ", ";
+		}
+		cout << "]";
+		if (i != arr.size() - 1)
+			cout << "," << endl;
+	}
+	cout << "], 2D Vector of Pairs\n";
+}
+
+
+template <typename t1, typename t2> void print(const pair<t1, t2> &p){
+	cout << "\n{" << p.first << "," << p.second << "}, Pair\n";
+}
+
+
+#endif
